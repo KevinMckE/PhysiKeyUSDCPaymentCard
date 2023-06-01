d63ecd5254a5a354e49f1489e68efbdd5bc624c4
KevinMckE <kevin@kevinecke.com>
2023-06-01 14:36:59 -0600

    Trying to do it without a Python Flask app first https://docs.moralis.io/web3-data-api/evm/how-to-get-all-nfts-by-wallet-addresses

--- src/backend/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-from dotenv import load_dotenv
-from moralis import evm_api
-import os
-
-load_dotenv()
-
-app = Flask(__name__)
-api_key = os.getenv("MORALIS_API_KEY")

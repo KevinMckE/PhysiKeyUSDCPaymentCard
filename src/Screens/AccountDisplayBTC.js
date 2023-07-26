9203f0ffde5f0c92e035d94cfc4643a3131f2722
KevinMckE <kevin@kevinecke.com>
2023-07-26 16:28:41 -0600

    Basic transaction outline built

--- src/Screens/AccountDisplayBTC.js
+++ src/Screens/AccountDisplayBTC.js
@@ -126,7 +126,10 @@ function AccountDisplayBTC(props) {
     if(encryptedPrivateKey != ''){
 
       let txObject = new bitcoin.TransactionBuilder(testnet);
-
+      txObject.addInput("d18e7106e5492baf8f3929d2d573d27d89277f3825d3836aa86ea1d843b5158b", 1); //UTXO to spend from
+      txObject.addOutput("12idKQBikRgRuZEbtxXQ4WFYB7Wa3hZzhT", 149000); //Address to send and amount to spend
+      txObject.sign(0, key); 
+      console.log(tx.build().toHex());
 
       
     } else {

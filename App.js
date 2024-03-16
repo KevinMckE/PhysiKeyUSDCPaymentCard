af8532a421106b010434cb7701bc2edbcbc33335
Mark <lisantimark@gmail.com>
2024-03-15 23:12:21 -0600

    dependency cleanup

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './shimeth.js';
+import './shim.js';
+import * as React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+// import AppNavigator from './src/components/AppNavigator.js';
+
+const App = () => {
+  return (
+    <PaperProvider>
+
+    </PaperProvider>
+);
+}
+
+export default App;

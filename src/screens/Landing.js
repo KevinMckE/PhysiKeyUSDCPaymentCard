9fb011b4f45f238822a58e720909bda6257eff17
Mark <lisantimark@gmail.com>
2024-03-16 23:14:05 -0600

    landing page done-ish

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,19 +1,30 @@
 import * as React from 'react';
-import { View, Image, StyleSheet, Text } from 'react-native';
-import CustomButton from '../components/NavigationButton';
+import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import NavigationButton from '../components/NavigationButton';
 
 //source={require('../assets/blob_background.png')}
 const Landing = ({ navigation }) => {
+
+  const handleLinkPress = () => {
+    Linking.openURL('https://anywhereaccess.io');
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.topContainer}>
+        <Image
+          source={require('../assets/blob_background.png')}
+          style={styles.backgroundImage}
+        />
         <Image
           source={require('../assets/card_rotate_animation.gif')}
           style={styles.centeredImage}
         />
       </View>
       <View style={styles.bottomContainer}>
-        <CustomButton navigation={navigation} text='Scan Card' type='primary' target='Login' size='large' />
+        <NavigationButton navigation={navigation} text='Scan Card' type='primary' target='Login' size='large' />
+        <Text style={styles.paragraphText}>Don't have a card?</Text>
+        <Text><TouchableOpacity onPress={handleLinkPress}><Text style={styles.linkText}>Order one here.</Text></TouchableOpacity></Text>
       </View>
     </View>
   );
@@ -27,15 +38,27 @@ const styles = StyleSheet.create({
     flex: 2,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundImage: require('../assets/blob_background.png')
+  },
+  backgroundImage: {
+    position: 'absolute',
   },
   centeredImage: {
-
+    width: '150%',
+    height: '150%'
   },
   bottomContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  linkText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+    fontSize: 18,
+  },
+  paragraphText: {
+    color: '#000000',
+    fontSize: 18,
   },
 });
 

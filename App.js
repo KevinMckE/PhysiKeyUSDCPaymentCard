556d6f2aa877782aa663cae21f0631212ecffbce
Mark <lisantimark@gmail.com>
2024-04-08 12:03:39 -0600

    remove console.logs

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
 import AppNavigator from './src/components/AppNavigator.js';
 
 const App = () => {
+ if (!__DEV__) {
+    console.log = () => {};
+  }
   return (
     <PaperProvider theme={DefaultTheme}>
       <AppNavigator />

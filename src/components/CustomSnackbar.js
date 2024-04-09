75ea10e0d65f6be672492eef057fa135edadc2ae
Mark <lisantimark@gmail.com>
2024-04-08 23:51:07 -0600

    snackbar quick fix

--- src/components/CustomSnackbar.js
+++ src/components/CustomSnackbar.js
@@ -1,13 +1,14 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Snackbar, Icon, Text } from 'react-native-paper';
 
 const CustomSnackbar = ({ visible, onDismiss, duration, text, isSuccess }) => {
   return (
+    <View style={styles.container}>
     <Snackbar
       visible={visible}
       onDismiss={onDismiss}
       duration={duration}
-      style={{ backgroundColor: '#333333' }}
+      style={styles.snackbar}
     >
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         {isSuccess ? (
@@ -18,7 +19,20 @@ const CustomSnackbar = ({ visible, onDismiss, duration, text, isSuccess }) => {
         <Text style={{ color: '#ffffff', marginLeft: 10 }}>{text}</Text>
       </View>
     </Snackbar>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  snackbar: {
+    backgroundColor: '#333333',
+  },
+});
 
 export default CustomSnackbar;
